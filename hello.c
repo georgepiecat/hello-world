b57695788745f4a9c12a3df5784f1e2c3f38d3b5
George <georgepiecat@mailinator.com>
2017-04-04 08:32:01 -0700

    improve code

--- hello.c
+++ hello.c
@@ -1,7 +1,17 @@
-#include <stdio.h"
+#include <stdio.h>
+
+#define SIZEOFNORMALCHARONMOSTCOMPUTERS 1
+
+void printf_wrap(void *ptr)
+{
+	char *str = (char*) ptr;
+	if (str != NULL && ptr != NULL && sizeof(char) == SIZEOFNORMALCHARONMOSTCOMPUTERS && &printf != NULL) {
+		printf(str);
+	}
+}
 
 int main()
 {
-	printf("\nhello world");
+	printf_wrap("\nhello world");
 	return 0;
 }
